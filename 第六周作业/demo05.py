9619c3a352dce03dd54459d1e6e62616034c4272
zzzzzsy <zzzzzsy@blue.com>
2022-04-08 17:15:04 +0800

    updata

--- 第六周作业/demo05.py
+++ 第六周作业/demo05.py
@@ -5,34 +5,56 @@ sum1=0
 sum2=0
 list=[]
 list_pick=[]
-for i in range(0,6):
-    x=random.randint(1,33)
-    for j in range(0,i):
-        while x<=list[j]:
-            x=random.randint(1, 33)
-    list.append(x)
-list.append((random.randint(1,16)))
+#选双色球
 print("1.机选")
 print("2.自定义输入")
-choice=input("请输入您的选择：")
+choice=int(input("请输入您的选择："))
+#机选
 if int(choice)==1:
     for i in range(0, 6):
         y = random.randint(1, 33)
-        for j in range(0, i):
-            while y <= list_pick[j]:
-                y=random.randint(1, 33)
-        list_pick.append(y)
-    list_pick.append((random.randint(1, 16)))
+        if y<10:
+            y='0'+str(y)
+        list_pick.append(str(y))
+    list_pick.sort()
+    y=random.randint(1, 16)
+    if int(y) < 10:
+        y = '0' + str(y)
+    list_pick.append(str(y))
     print("机选双色球为{}".format(list_pick))
+#自定义选择
 elif int(choice)==2:
-    for i in range(0,7):
-        list_pick.append(input())
-print("中奖双色球为{}".format(list))
+    for i in range(0,6):
+        y=eval(input("请输入红色球的号码，范围在1-33："))
+        if y<10:
+            y='0'+str(y)
+        list_pick.append(str(y))
+    list_pick.sort()
+    y = eval(input("请输入蓝色球号码，范围在1-16："))
+    if y < 10:
+        y = '0' + str(y)
+    list_pick.append(str(y))
+    print("自选双色球为{}".format(list_pick))
+
+#开奖
 for i in range(0,6):
-    if int(list[i])==(list_pick[i]):
+    x=random.randint(1,33)
+    if x < 10:
+        x = '0' + str(x)
+    list.append(str(x))
+list.sort()
+x=random.randint(1,16)
+if x < 10:
+    x = '0' + str(x)
+list.append(str(x))
+print("中奖双色球为{}".format(list))
+#统计中奖个数
+for i in range(0,6):
+    if str(list[i])==str(list_pick[i]):
         sum1=sum1+1
-if int(list[6])==int(list_pick[6]):
+if str(list[6])==str(list_pick[6]):
     sum2=1
+#判断奖金
 if sum1<=2 and sum2==1:
     print("奖金5元！")
 elif sum1+sum2==4:
@@ -45,10 +67,5 @@ elif sum1==6:
     print("奖金10万元！")
 elif sum1==6 and sum2==1:
     print("奖金500万元！")
-
-
-
-
-
-
-
+else:
+    print("很遗憾您未中奖！")
